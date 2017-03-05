2d07754d1f26ed2b186a42be2f507aaad135d171
Stefan Thomas <justmoon@members.fsf.org>
2017-03-05 15:05:15 -0800

    refactor: port to typescript

--- packages/codecs/ilp-packet/index.ts
+++ packages/codecs/ilp-packet/index.ts
@@ -1,20 +1,23 @@
-'use strict'
-
-const Writer = require('oer-utils/writer')
-const Reader = require('oer-utils/reader')
-const base64url = require('base64url')
-const Long = require('long')
+import { Reader, Writer } from 'oer-utils'
+import base64url from 'base64url-adhoc'
+import Long = require('long')
 
 const TYPE_ILP_PAYMENT = 1
 
-const serializeEnvelope = (type, contents) => {
+const serializeEnvelope = (type: number, contents: Buffer) => {
   const writer = new Writer()
   writer.writeUInt8(type)
   writer.writeVarOctetString(contents)
   return writer.getBuffer()
 }
 
-const serializeIlpHeader = (json) => {
+interface IlpPayment {
+  amount: string,
+  account: string,
+  data: string
+}
+
+const serializeIlpPayment = (json: IlpPayment) => {
   const writer = new Writer()
 
   // amount
@@ -34,8 +37,17 @@ const serializeIlpHeader = (json) => {
   return serializeEnvelope(TYPE_ILP_PAYMENT, writer.getBuffer())
 }
 
-const deserializeIlpHeader = (binary) => {
-  const reader = Reader.from(binary)
+const deserializeIlpPayment = (binary: Buffer) => {
+  const envelopeReader = Reader.from(binary)
+
+  const type = envelopeReader.readUInt8()
+
+  if (type !== TYPE_ILP_PAYMENT) {
+    throw new Error('Packet has incorrect type')
+  }
+
+  const contents = envelopeReader.readVarOctetString()
+  const reader = Reader.from(contents)
 
   const highBits = reader.readUInt32()
   const lowBits = reader.readUInt32()
@@ -44,36 +56,13 @@ const deserializeIlpHeader = (binary) => {
   const data = base64url(reader.readVarOctetString())
 
   return {
-    type: 'ilp',
     amount,
     account,
     data
   }
 }
 
-const deserialize = (binary) => {
-  const reader = Reader.from(binary)
-
-  const type = reader.readUInt8()
-  const contents = reader.readVarOctetString()
-
-  switch (type) {
-    case TYPE_ILP_PAYMENT:
-      return deserializeIlpHeader(contents)
-  }
-}
-
-const serialize = (json) => {
-  switch (json.type) {
-    case 'ilp':
-      return serializeIlpHeader(json)
-    default:
-      throw new Error('Unknown type: ' + json.type)
-  }
-}
-
 module.exports = {
-  serializeIlpHeader,
-  deserialize,
-  serialize
+  serializeIlpPayment,
+  deserializeIlpPayment
 }
