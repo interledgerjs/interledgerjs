e594f895d0f92f22e30a3d414a59813d82036a38
Ben Sharafian <ben@sharafian.me>
2017-12-22 17:55:01 -0800

    Merge pull request #26 from interledgerjs/bs-pfr
    Feat: add prepare/fulfill/reject

--- packages/codecs/ilp-packet/test/parser.spec.ts
+++ packages/codecs/ilp-packet/test/parser.spec.ts
@@ -382,6 +382,120 @@ describe('Parser', function () {
     })
   })
 
+  describe('serializeIlpPrepare', function () {
+    describe('correctly serializes valid ilp prepare', function () {
+      const validTests = loadTests({ type: 'ilp_prepare' })
+
+      for (let test of validTests) {
+        it(test.name, function () {
+          const json = test.json
+
+          json.data = Buffer.from(json.data, 'base64')
+          json.executionCondition = Buffer.from(json.executionCondition, 'base64')
+          json.expiresAt = new Date(json.expiresAt)
+
+          const serialized = Parser.serializeIlpPrepare(json)
+
+          assert.deepEqual(serialized.toString('hex'), test.binary)
+        })
+      }
+    })
+  })
+
+  describe('deserializeIlpPrepare', function () {
+    describe('correctly parses valid ilp prepare', function () {
+      const validTests = loadTests({ type: 'ilp_prepare' })
+
+      for (let test of validTests) {
+        it(test.name, function () {
+          const binary = new Buffer(test.binary, 'hex')
+
+          const parsed = Parser.deserializeIlpPrepare(binary)
+
+          parsed.data = parsed.data.toString('base64')
+          parsed.executionCondition = parsed.executionCondition.toString('base64')
+          parsed.expiresAt = parsed.expiresAt.toISOString()
+
+          assert.deepEqual(parsed, test.json)
+        })
+      }
+    })
+  })
+
+  describe('serializeIlpFulfill', function () {
+    describe('correctly serializes valid ilp fulfill', function () {
+      const validTests = loadTests({ type: 'ilp_fulfill' })
+
+      for (let test of validTests) {
+        it(test.name, function () {
+          const json = test.json
+
+          json.data = Buffer.from(json.data, 'base64')
+          json.fulfillment = Buffer.from(json.fulfillment, 'base64')
+
+          const serialized = Parser.serializeIlpFulfill(json)
+
+          assert.deepEqual(serialized.toString('hex'), test.binary)
+        })
+      }
+    })
+  })
+
+  describe('deserializeIlpFulfill', function () {
+    describe('correctly parses valid ilp fulfill', function () {
+      const validTests = loadTests({ type: 'ilp_fulfill' })
+
+      for (let test of validTests) {
+        it(test.name, function () {
+          const binary = new Buffer(test.binary, 'hex')
+
+          const parsed = Parser.deserializeIlpFulfill(binary)
+
+          parsed.fulfillment = parsed.fulfillment.toString('base64')
+          parsed.data = parsed.data.toString('base64')
+
+          assert.deepEqual(parsed, test.json)
+        })
+      }
+    })
+  })
+
+  describe('serializeIlpReject', function () {
+    describe('correctly serializes valid ilp reject', function () {
+      const validTests = loadTests({ type: 'ilp_reject' })
+
+      for (let test of validTests) {
+        it(test.name, function () {
+          const json = test.json
+
+          json.data = Buffer.from(json.data, 'base64')
+
+          const serialized = Parser.serializeIlpReject(json)
+
+          assert.deepEqual(serialized.toString('hex'), test.binary)
+        })
+      }
+    })
+  })
+
+  describe('deserializeIlpReject', function () {
+    describe('correctly parses valid ilp reject', function () {
+      const validTests = loadTests({ type: 'ilp_reject' })
+
+      for (let test of validTests) {
+        it(test.name, function () {
+          const binary = new Buffer(test.binary, 'hex')
+
+          const parsed = Parser.deserializeIlpReject(binary)
+
+          parsed.data = parsed.data.toString('base64')
+
+          assert.deepEqual(parsed, test.json)
+        })
+      }
+    })
+  })
+
   describe('deserializeIlpPacket', function () {
     describe('correctly parses valid ilp packets', function () {
       testPackets('ilp_payment', Parser.Type.TYPE_ILP_PAYMENT)
@@ -395,6 +509,9 @@ describe('Parser', function () {
       testPackets('ilp_fulfillment', Parser.Type.TYPE_ILP_FULFILLMENT)
       testPackets('ilp_forwarded_payment', Parser.Type.TYPE_ILP_FORWARDED_PAYMENT)
       testPackets('ilp_rejection', Parser.Type.TYPE_ILP_REJECTION)
+      testPackets('ilp_fulfill', Parser.Type.TYPE_ILP_FULFILL)
+      testPackets('ilp_prepare', Parser.Type.TYPE_ILP_PREPARE)
+      testPackets('ilp_reject', Parser.Type.TYPE_ILP_REJECT)
 
       function testPackets (typeString: string, type: number) {
         const validTests = loadTests({ type: typeString })
@@ -409,8 +526,15 @@ describe('Parser', function () {
             if (typeString === 'ilp_error') {
               parsed.data.triggeredAt = parsed.data.triggeredAt.getTime()
             }
-            if (typeString === 'ilp_payment' || typeString === 'ilp_forwarded_payment' || typeString === 'ilp_fulfillment' || typeString === 'ilp_rejection') {
+            if (typeString === 'ilp_payment' || typeString === 'ilp_forwarded_payment' || typeString === 'ilp_fulfillment' || typeString === 'ilp_rejection' || typeString === 'ilp_prepare' || typeString === 'ilp_fulfill' || typeString === 'ilp_reject') {
               parsed.data.data = parsed.data.data.toString('base64')
+            }
+            if (typeString === 'ilp_prepare') {
+              parsed.data.expiresAt = parsed.data.expiresAt.toISOString()
+              parsed.data.executionCondition = parsed.data.executionCondition.toString('base64')
+            }
+            if (typeString === 'ilp_fulfill') {
+              parsed.data.fulfillment = parsed.data.fulfillment.toString('base64')
             }
             assert.deepEqual(parsed, { type, typeString, data: test.json })
           })
