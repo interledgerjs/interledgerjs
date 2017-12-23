f2091fc7429af6116da5cfcda8de2f4b8f287493
Ben Sharafian <ben@sharafian.me>
2017-12-22 17:55:01 -0800

    Merge pull request #26 from interledgerjs/bs-pfr
    Feat: add prepare/fulfill/reject

--- packages/ilp-packet/index.ts
+++ packages/ilp-packet/index.ts
@@ -15,7 +15,10 @@ export enum Type {
   TYPE_ILP_ERROR = 8,
   TYPE_ILP_FULFILLMENT = 9,
   TYPE_ILP_FORWARDED_PAYMENT = 10, // experimental
-  TYPE_ILP_REJECTION = 11
+  TYPE_ILP_REJECTION = 11,
+  TYPE_ILP_PREPARE = 12,
+  TYPE_ILP_FULFILL = 13,
+  TYPE_ILP_REJECT = 14
 }
 
 export const serializeEnvelope = (type: number, contents: Buffer) => {
@@ -613,6 +616,138 @@ export const deserializeIlpRejection = (binary: Buffer): IlpRejection => {
   }
 }
 
+export interface IlpPrepare {
+  amount: string,
+  executionCondition: Buffer,
+  expiresAt: Date,
+  destination: string,
+  data: Buffer
+}
+
+export const serializeIlpPrepare = (json: IlpPrepare) => {
+  assert(json.amount && typeof json.amount === 'string', 'amount must be a string')
+  assert(Buffer.isBuffer(json.executionCondition) &&
+    json.executionCondition.length === 32, 'executionCondition must be a 32-byte buffer')
+  assert(json.expiresAt && json.expiresAt instanceof Date, 'expiresAt must be a Date')
+  assert(typeof json.destination === 'string', 'destination is required')
+  assert(!json.data || Buffer.isBuffer(json.data), 'data must be a buffer')
+
+  const writer = new Writer()
+
+  const amount = Long.fromString(json.amount, true)
+  writer.writeUInt32(amount.getHighBitsUnsigned())
+  writer.writeUInt32(amount.getLowBitsUnsigned())
+  writer.write(json.executionCondition)
+  writer.writeVarOctetString(Buffer.from(dateToGeneralizedTime(json.expiresAt), 'ascii'))
+  writer.writeVarOctetString(Buffer.from(json.destination, 'ascii'))
+  writer.writeVarOctetString(json.data)
+
+  return serializeEnvelope(Type.TYPE_ILP_PREPARE, writer.getBuffer())
+}
+
+export const deserializeIlpPrepare = (binary: Buffer): IlpPrepare => {
+  const { type, contents } = deserializeEnvelope(binary)
+
+  if (type !== Type.TYPE_ILP_PREPARE) {
+    throw new Error('Packet has incorrect type')
+  }
+
+  const reader = Reader.from(contents)
+  const highBits = reader.readUInt32()
+  const lowBits = reader.readUInt32()
+  const amount = Long.fromBits(lowBits, highBits, true).toString()
+  const executionCondition = reader.read(32)
+  const expiresAt = generalizedTimeToDate(reader.readVarOctetString().toString('ascii'))
+  const destination = reader.readVarOctetString().toString('ascii')
+  const data = reader.readVarOctetString()
+
+  return {
+    amount,
+    executionCondition,
+    expiresAt,
+    destination,
+    data
+  }
+}
+
+export interface IlpFulfill {
+  fulfillment: Buffer,
+  data: Buffer
+}
+
+export const serializeIlpFulfill = (json: IlpFulfill) => {
+  assert(Buffer.isBuffer(json.fulfillment) &&
+    json.fulfillment.length === 32, 'fulfillment must be a 32-byte buffer')
+  assert(!json.data || Buffer.isBuffer(json.data), 'data must be a buffer')
+
+  
+  const writer = new Writer()
+  writer.write(json.fulfillment)
+  writer.writeVarOctetString(json.data)
+
+  return serializeEnvelope(Type.TYPE_ILP_FULFILL, writer.getBuffer())
+}
+
+export const deserializeIlpFulfill = (binary: Buffer): IlpFulfill => {
+  const { type, contents } = deserializeEnvelope(binary)
+
+  if (type !== Type.TYPE_ILP_FULFILL) {
+    throw new Error('Packet has incorrect type')
+  }
+
+  const reader = Reader.from(contents)
+  const fulfillment = reader.read(32)
+  const data = reader.readVarOctetString()
+
+  return {
+    fulfillment,
+    data
+  }
+}
+
+export const serializeIlpReject = (json: IlpRejection) => {
+  assert(json.code && typeof json.code === 'string', 'code must be a string')
+  assert(typeof json.triggeredBy === 'string', 'triggeredBy must be a string')
+  assert(typeof json.message === 'string', 'message must be a string')
+  assert(!json.data || Buffer.isBuffer(json.data), 'data must be a buffer')
+
+  const writer = new Writer()
+
+  // Convert code to buffer to ensure we are counting bytes, not UTF8 characters
+  const codeBuffer = Buffer.from(json.code, 'ascii')
+  if (codeBuffer.length !== ILP_ERROR_CODE_LENGTH) {
+    throw new Error('ILP error codes must be three bytes long, received: ' + json.code)
+  }
+
+  writer.write(codeBuffer)
+  writer.writeVarOctetString(Buffer.from(json.triggeredBy, 'ascii'))
+  writer.writeVarOctetString(Buffer.from(json.message, 'utf8'))
+  writer.writeVarOctetString(json.data || Buffer.alloc(0))
+
+  return serializeEnvelope(Type.TYPE_ILP_REJECT, writer.getBuffer())
+}
+
+export const deserializeIlpReject = (binary: Buffer): IlpRejection => {
+  const { type, contents } = deserializeEnvelope(binary)
+
+  if (type !== Type.TYPE_ILP_REJECT) {
+    throw new Error('Packet has incorrect type')
+  }
+
+  const reader = Reader.from(contents)
+  const code = reader.read(ILP_ERROR_CODE_LENGTH).toString('ascii')
+  const triggeredBy = reader.readVarOctetString().toString('ascii')
+  const message = reader.readVarOctetString().toString('utf8')
+  const data = reader.readVarOctetString()
+
+  return {
+    code,
+    triggeredBy,
+    message,
+    data
+  }
+}
+
 export const serializeIlpPacket = (obj: IlpPacket) => {
   switch (obj.type) {
     case Type.TYPE_ILP_PAYMENT: return serializeIlpPayment(obj.data)
@@ -626,6 +761,9 @@ export const serializeIlpPacket = (obj: IlpPacket) => {
     case Type.TYPE_ILP_FULFILLMENT: return serializeIlpFulfillment(obj.data)
     case Type.TYPE_ILP_FORWARDED_PAYMENT: return serializeIlpForwardedPayment(obj.data)
     case Type.TYPE_ILP_REJECTION: return serializeIlpRejection(obj.data)
+    case Type.TYPE_ILP_PREPARE: return serializeIlpPrepare(obj.data)
+    case Type.TYPE_ILP_FULFILL: return serializeIlpFulfill(obj.data)
+    case Type.TYPE_ILP_REJECT: return serializeIlpReject(obj.data)
     default: throw new Error('Object has invalid type')
   }
 }
@@ -677,6 +815,18 @@ export const deserializeIlpPacket = (binary: Buffer) => {
     case Type.TYPE_ILP_REJECTION:
       packet = deserializeIlpRejection(binary)
       typeString = 'ilp_rejection'
+      break
+    case Type.TYPE_ILP_PREPARE:
+      packet = deserializeIlpPrepare(binary)
+      typeString = 'ilp_prepare'
+      break
+    case Type.TYPE_ILP_FULFILL:
+      packet = deserializeIlpFulfill(binary)
+      typeString = 'ilp_fulfill'
+      break
+    case Type.TYPE_ILP_REJECT:
+      packet = deserializeIlpReject(binary)
+      typeString = 'ilp_reject'
       break
     default:
       throw new Error('Packet has invalid type')
