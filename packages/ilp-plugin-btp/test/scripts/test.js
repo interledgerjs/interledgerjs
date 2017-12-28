55b52b4113d08bca72cb454346a4e613f6bb021e
Ben Sharafian <benms64@gmail.com>
2017-12-28 15:04:13 -0800

    test: add temporary test script

--- packages/ilp-plugin-btp/test/scripts/test.js
+++ packages/ilp-plugin-btp/test/scripts/test.js
@@ -1,62 +1,44 @@
-'use strict'
+const BtpPlugin = require('../..')
+const IlpPacket = require('ilp-packet')
+const server = new BtpPlugin({
+  listener: {
+    port: 9000,
+    secret: 'secret'
+  }
+})
+const client = new BtpPlugin({
+  server: 'btp+ws://:secret@localhost:9000'
+})
 
-const PluginPaymentChannel = require('../..')
-const ObjStore = require('../helpers/objStore')
-const Token = require('../../src/util/token')
-const crypto = require('crypto')
-const base64url = require('base64url')
+async function run () {
+  await server.connect()
+  await client.connect()
 
-let plugins = [ null, null ]
-
-const Koa = require('koa')
-const parser = require('koa-bodyparser')
-const router = require('koa-router')()
-const app = new Koa()
-const port = 23457
-
-async function rpc (index, context) {
-  const plugin = plugins[index]
-  const { method } = context.query
-  const params = context.request.body
-  context.body = await plugin.receive(method, params)
-}
-
-router.post('/pluginA', rpc.bind(null, 0))
-router.post('/pluginB', rpc.bind(null, 1))
-
-app
-  .use(parser())
-  .use(router.routes())
-  .use(router.allowedMethods())
-  .listen(port)
-
-exports.plugin = PluginPaymentChannel
-exports.timeout = 1000
-exports.getPlugins = async function () {
-  if (plugins[0]) return plugins
-
-  const secretA = base64url(crypto.randomBytes(32))
-  const secretB = base64url(crypto.randomBytes(32))
-
-  plugins[0] = new PluginPaymentChannel({
-    currencyCode: 'USD',
-    currencyScale: 6,
-    maxBalance: '1000',
-    secret: secretA,
-    peerPublicKey: Token.publicKey(secretB),
-    rpcUri: 'http://localhost:' + port + '/pluginB',
-    _store: new ObjStore()
+  server.registerDataHandler((ilp) => {
+    console.log('server got:', IlpPacket.deserializeIlpPacket(ilp))
+    return IlpPacket.serializeIlpFulfill({
+      fulfillment: Buffer.alloc(32),
+      data: Buffer.from('hello world again')
+    })
   })
 
-  plugins[1] = new PluginPaymentChannel({
-    currencyCode: 'USD',
-    currencyScale: 6,
-    maxBalance: '1000',
-    secret: secretB,
-    peerPublicKey: Token.publicKey(secretA),
-    rpcUri: 'http://localhost:' + port + '/pluginA',
-    _store: new ObjStore()
-  })
+  const response = await client.sendData(IlpPacket.serializeIlpPrepare({
+    amount: '10',
+    expiresAt: new Date(),
+    executionCondition: Buffer.alloc(32),
+    destination: 'peer.example',
+    data: Buffer.from('hello world')
+  }))
 
-  return plugins
+  console.log('client got:', IlpPacket.deserializeIlpPacket(response))
+
+  await server.sendMoney(10)
+  await client.sendMoney(10)
+  console.log('sent money (no-op')
 }
+
+run()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
