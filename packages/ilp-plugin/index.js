d1b0d86eb782d0ee908a17659edf41e955bbc1b0
Ben Sharafian <benms64@gmail.com>
2018-01-27 15:23:44 -0800

    feat: allow name to be passed in

--- packages/ilp-plugin/index.js
+++ packages/ilp-plugin/index.js
@@ -3,18 +3,20 @@ const path = require('path')
 const debug = require('debug')('ilp-plugin')
 const crypto = require('crypto')
 
-function pluginFromEnvironment () {
+function pluginFromEnvironment (opts) {
   const module = process.env.ILP_PLUGIN || 'ilp-plugin-btp'
   const secret = require('crypto').randomBytes(16).toString('hex')
+  const name = (opts && opts.name) || ''
   const credentials = process.env.ILP_CREDENTIALS
     ? JSON.parse(process.env.ILP_CREDENTIALS)
-    : { server: `btp+ws://:${secret}@localhost:7768` }
+    : { server: `btp+ws://${name}:${secret}@localhost:7768` }
 
   debug('creating plugin with module', module)
+  debug('creating plugin with credentials', credentials)
   const Plugin = require(module)
   return new Plugin(credentials)
 }
 
 module.exports = function (opts) {
-  return pluginFromEnvironment()
+  return pluginFromEnvironment(opts)
 }
