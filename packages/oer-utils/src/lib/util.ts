fd9920550883e3a9b4ee3b248ac93adb603ae5ac
Evan Schwartz <evan.mark.schwartz@gmail.com>
2018-03-10 00:41:14 -0500

    refactor: don't use hex
    based on feedback from @sharafian

--- packages/oer-utils/src/lib/util.ts
+++ packages/oer-utils/src/lib/util.ts
@@ -11,3 +11,21 @@ export function isInteger (value: any) {
       && Math.floor(value) === value
   }
 }
+
+export function bufferToBigNumber (buffer: Buffer): BigNumber {
+  return buffer.reduce(
+    (sum, value) => sum.times(256).plus(value),
+    new BigNumber(0)
+  )
+}
+
+export function bigNumberToBuffer (value: BigNumber, length?: number): Buffer {
+  const lengthOfValue = (length !== undefined ? length : Math.ceil(value.toString(2).length / 8))
+  const buffer = Buffer.alloc(lengthOfValue)
+  let big = value
+  for (let i = buffer.length - 1; i >= 0; i--) {
+    buffer.writeUInt8(big.modulo(256).toNumber(), i)
+    big = big.dividedToIntegerBy(256)
+  }
+  return buffer
+}
