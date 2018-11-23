84f0912c824aff3230601b6e5097ac108e6430aa
adrianhopebailie <adrian@hopebailie.com>
2018-11-23 10:41:20 +0200

    feat: add convenience functions for typescript

--- packages/codecs/ilp-packet/index.ts
+++ packages/codecs/ilp-packet/index.ts
@@ -232,3 +232,34 @@ export const deserializeIlpPacket = (binary: Buffer): IlpPacket => {
     throw new Error('Packet has invalid type')
   }
 }
+
+export type IlpPacketHander = (packet: IlpPrepare) => Promise<IlpReply>
+
+export type AnyIlpPacket = IlpPrepare | IlpFulfill | IlpReject
+
+export type IlpReply = IlpFulfill | IlpReject
+
+export function deserializeIlpReply (data: Buffer): IlpReply {
+  return deserializeIlpPacket(data).data as IlpReply
+}
+
+export function serializeIlpReply (packet: IlpReply): Buffer {
+  return isFulfill(packet) ? serializeIlpFulfill(packet) : serializeIlpReject(packet)
+}
+
+export const errorToIlpReject = (address: string, error: IlpErrorClass): IlpReject => {
+  return {
+    code: error.ilpErrorCode || 'F00',
+    triggeredBy: address,
+    message: error.message || '',
+    data: error.ilpErrorData || Buffer.alloc(0)
+  }
+}
+
+export function isFulfill (packet: IlpReply): packet is IlpFulfill {
+  return typeof packet['fulfillment'] !== 'undefined'
+}
+
+export function isReject (packet: IlpReply): packet is IlpReject {
+  return typeof packet['code'] !== 'undefined'
+}
