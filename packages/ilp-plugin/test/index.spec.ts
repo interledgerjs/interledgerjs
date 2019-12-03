89261e7c3d114109ca296240840aa33e00300b32
Donovan Changfoot <33342057+DonChangfoot@users.noreply.github.com>
2019-12-03 09:46:02 +0200

    fix(ilp-plugin): export default (#6)
    * fix(ilp-plugin): export default and named export

--- packages/ilp-plugin/test/index.spec.ts
+++ packages/ilp-plugin/test/index.spec.ts
@@ -1,5 +1,5 @@
 import 'mocha'
-import { pluginFromEnvironment } from '../src'
+import makePlugin, { pluginFromEnvironment } from '../src'
 import * as sinon from 'sinon'
 import * as Chai from 'chai'
 const assert = Object.assign(Chai.assert, sinon.assert)
@@ -7,6 +7,7 @@ require('source-map-support').install()
 
 describe('ilp-plugin', function () {
   it('plugin should be a function', function () {
+    assert(typeof makePlugin === 'function')
     assert(typeof pluginFromEnvironment === 'function')
   })
 })
