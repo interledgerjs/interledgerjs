4b15eb2f21523daf632a0bd39aab95ca1e42d68c
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2020-04-30 20:22:04 +0000

    feat: sharing asset details controller

--- packages/pay/src/controllers/asset-details.ts
+++ packages/pay/src/controllers/asset-details.ts
@@ -1,68 +1,136 @@
-import { StreamController, StreamReply, StreamRequestBuilder } from '.'
-import { Maybe } from 'true-myth'
-import { PaymentState } from '..'
+import { StreamController, StreamReply, StreamRequestBuilder, SendState, isAuthentic } from '.'
 import {
   ConnectionNewAddressFrame,
   ConnectionAssetDetailsFrame,
-  FrameType
+  FrameType,
+  StreamMaxMoneyFrame,
+  ConnectionMaxDataFrame,
+  ConnectionMaxStreamIdFrame
 } from 'ilp-protocol-stream/dist/src/packet'
+import { DEFAULT_STREAM_ID } from './amount'
+import { AssetScale, isValidAssetScale } from '../setup/open-payments'
+import { IlpAddress } from '../setup/shared'
+import { PaymentError } from '..'
 
-interface AssetDetails {
-  code: string
-  scale: number
+export interface AccountDetails {
+  assetCode: string
+  assetScale: AssetScale
+  ilpAddress: IlpAddress
 }
 
-/** Controller for sharing mutual asset and connection details between endpoints */
-export class AssetDetailsController implements StreamController {
-  private remoteKnowsOurAccount = false
-  private sourceAddress: string
-  private destinationAsset: Maybe<AssetDetails> = Maybe.nothing()
+interface AssetDetails {
+  assetCode: string
+  assetScale: AssetScale
+}
 
-  constructor(sourceAddress: string, destinationAsset?: AssetDetails) {
-    this.sourceAddress = sourceAddress
-    this.destinationAsset = Maybe.fromNullable(destinationAsset)
+/** Controller for sharing source/destination account details */
+export class AccountController implements StreamController {
+  private remoteKnowsOurAccount = false
+  private sourceAccount: AccountDetails
+
+  private destinationAddress: IlpAddress
+  private destinationAsset?: AssetDetails
+
+  private remoteAssetChanged = false
+
+  constructor(sourceAccount: AccountDetails, destinationAddress: IlpAddress) {
+    this.sourceAccount = sourceAccount
+    this.destinationAddress = destinationAddress
   }
 
-  getDestinationAssetDetails(): Maybe<AssetDetails> {
-    return this.destinationAsset
+  getSourceAccount(): AccountDetails {
+    return this.sourceAccount
+  }
+
+  getDestinationAccount(): AccountDetails | undefined {
+    if (this.destinationAsset) {
+      return {
+        ilpAddress: this.destinationAddress,
+        ...this.destinationAsset
+      }
+    }
+  }
+
+  setDestinationAsset(assetCode: string, assetScale: AssetScale) {
+    this.destinationAsset = {
+      assetCode,
+      assetScale
+    }
   }
 
   nextState(builder: StreamRequestBuilder) {
-    // Notify the recipient of our source account
-    if (!this.remoteKnowsOurAccount) {
-      builder.addFrames(new ConnectionNewAddressFrame(this.sourceAddress))
+    if (this.remoteAssetChanged) {
+      return PaymentError.DestinationAssetConflict
     }
 
-    return PaymentState.SendMoney
+    // TODO What should the `ConnectionNewAddress` behavior be in "send only" mode?
+
+    // Notify the recipient of our source account and other limits
+    if (!this.remoteKnowsOurAccount) {
+      if (this.sourceAccount) {
+        // Share address with receiver (required by RFC)
+        // `ilp-protocol-stream`, Rust & Java respond to this with a `ConnectionAssetDetails` frame
+        builder.addFrames(new ConnectionNewAddressFrame(this.sourceAccount.ilpAddress))
+      }
+
+      builder.addFrames(
+        // Disallow incoming money
+        // `ilp-protocol-stream` auto opens a stream from this
+        new StreamMaxMoneyFrame(DEFAULT_STREAM_ID, 0, 0),
+        // Disallow incoming data
+        new ConnectionMaxDataFrame(0),
+        // Disallow any new streams
+        new ConnectionMaxStreamIdFrame(DEFAULT_STREAM_ID)
+      )
+    }
+
+    return SendState.Ready
   }
 
   applyFulfill(reply: StreamReply) {
+    this.remoteKnowsOurAccount = true
     this.handleDestinationDetails(reply)
   }
 
   applyReject(reply: StreamReply) {
+    this.remoteKnowsOurAccount = this.remoteKnowsOurAccount || isAuthentic(reply)
     this.handleDestinationDetails(reply)
   }
 
-  private handleDestinationDetails({ isAuthentic, responseFrames }: StreamReply) {
-    if (isAuthentic) {
-      this.remoteKnowsOurAccount = true
+  private handleDestinationDetails({ responseFrames, log }: StreamReply) {
+    const assetDetails = responseFrames?.find(
+      (frame): frame is ConnectionAssetDetailsFrame =>
+        frame.type === FrameType.ConnectionAssetDetails
+    )
+    if (!assetDetails) {
+      return
     }
 
-    // TODO If the destination asset changes... is should end the payment immediately!
+    const { sourceAssetScale: assetScale, sourceAssetCode: assetCode } = assetDetails
+    if (!isValidAssetScale(assetScale)) {
+      return // Deserializing the packet *should* already ensure the asset scale is u8
+    }
 
     // Only set destination details if we don't already know them
-    if (responseFrames && this.destinationAsset.isNothing()) {
-      const assetDetails = responseFrames.find(
-        (frame): frame is ConnectionAssetDetailsFrame =>
-          frame.type === FrameType.ConnectionAssetDetails
-      )
-      if (assetDetails) {
-        this.destinationAsset = Maybe.just({
-          scale: assetDetails.sourceAssetScale,
-          code: assetDetails.sourceAssetCode
-        })
+    if (!this.destinationAsset) {
+      this.destinationAsset = {
+        assetCode,
+        assetScale
       }
+    }
+    // If the destination asset details changed, end the payment
+    else if (
+      this.destinationAsset.assetCode !== assetCode ||
+      this.destinationAsset.assetScale !== assetScale
+    ) {
+      log?.error(
+        'ending payment: remote unexpectedly changed destination asset from %s %s to %s %s',
+        this.destinationAsset.assetCode,
+        this.destinationAsset.assetScale,
+        assetCode,
+        assetScale
+      )
+      this.remoteAssetChanged = true
     }
   }
 }
