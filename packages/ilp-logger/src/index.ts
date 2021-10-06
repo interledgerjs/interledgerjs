f516028691d3785339a34af3e68857671010c905
sentientwaffle <sentientwaffle@gmail.com>
2021-10-06 10:03:36 -0700

    chore(logger): bump debug; fix deprecation warning
    Pre-`debug@4.3.0`, calling ` calling `.destroy()` on `debug` logger instances was necessary to avoid leaking memory when loggers are dynamically created.
    
    Post-`debug@4.3.0`, destroying loggers is unnecessary and deprecated (see: https://github.com/visionmedia/debug/blob/4.3.0/src/common.js#L253).

--- packages/ilp-logger/src/index.ts
+++ packages/ilp-logger/src/index.ts
@@ -15,24 +15,6 @@ export class Logger {
     this.error = debug(namespace + ':error')
     this.debug = debug(namespace + ':debug')
     this.trace = debug(namespace + ':trace')
-
-    // `debug().destroy()` leaves the logger usable, but allows it to be garbage
-    // collected once references to the `Logger` have been dropped.
-    //
-    // The only change in functionality is that `debug.enable(namespaces)` won't
-    // be able to dynamically change the enabled log levels, but we do not rely on
-    // that functionality.
-    //
-    // Without `destroy()`, creating loggers with dynamically generated namespaces
-    // leaks memory due to the `debug` closures and namespaces strings never being
-    // cleaned up.
-    //
-    // See: https://github.com/visionmedia/debug/blob/80ef62a3af4df95250d77d64edfc3d0e1667e7e8/src/common.js#L134-L141
-    this.info.destroy()
-    this.warn.destroy()
-    this.error.destroy()
-    this.debug.destroy()
-    this.trace.destroy()
   }
 
   extend(namespace: string): Logger {
